4c3c1d847aa09fc29e15a68f8c3b9e7bb4389c2d
ostenilneto <ostenilneto@hotmail.com>
2024-02-03 12:50:52 -0300

    Pesquisa pelo nome e copia para outra pasta - XML/Delphi

---
+++ CopiaArquivos.dpr
@@ -0,0 +1,14 @@
+program CopiaArquivos;
+
+uses
+  Vcl.Forms,
+  Programa in 'Programa.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
